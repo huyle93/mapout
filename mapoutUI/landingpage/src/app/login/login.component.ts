5b8c4ff5d2a0f099fa671f4187eeab933b8e06bf
ab2010 <ab116893@gmail.com>
2018-07-25 23:32:55 -0400

    basic login and registration

---
+++ mapoutUI/landingpage/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  loginuser(event){
+    console.log(event)
+
+  }
+
+}
